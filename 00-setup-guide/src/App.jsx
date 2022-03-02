0ac597997c23760a7862fc0f83553dea72a87a7f
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-03-02 16:46:16 -0600

    Begin working on the starter code for a Three.js project.

---
+++ 00-setup-guide/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hello world</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
