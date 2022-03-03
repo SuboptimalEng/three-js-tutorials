903cb2333e91c6278bd645f245e9337c41e3ac9a
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-03-03 16:09:01 -0600

    Refactor three.js setup code.

--- 00-setup-guide/src/App.jsx
+++ 00-setup-guide/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect } from 'react';
+
+import * as THREE from 'three';
+
+import SceneInit from './lib/SceneInit';
 
 function App() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    test.scene.add(boxMesh);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>hello world</div>
-      </header>
+    <div>
+      <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
