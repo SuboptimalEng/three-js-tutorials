f6ca93ebabce66a4c6dd7d4d20ac6489ea2fcb62
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-03-25 14:29:06 -0500

    Add point light.

--- 03-lighting/src/App.jsx
+++ 03-lighting/src/App.jsx
@@ -48,9 +48,10 @@ function App() {
     const al = new THREE.AmbientLight(0xffffff, 0.5);
     test.scene.add(al);
 
-    // set up ambient light settings
+    // set up ambient light gui
     const alFolder = gui.addFolder('ambient light');
     const alSettings = { color: al.color.getHex() };
+    alFolder.add(al, 'visible');
     alFolder.add(al, 'intensity', 0, 1, 0.25);
     alFolder
       .addColor(alSettings, 'color')
@@ -58,13 +59,12 @@ function App() {
     alFolder.open();
 
     // setup directional light + helper
-    const dl = new THREE.DirectionalLight(0xffffff, 0.5);
+    const dl = new THREE.DirectionalLight(0xff0000, 0.5);
     dl.position.set(0, 2, 0);
-    test.scene.add(dl);
     const dlHelper = new THREE.DirectionalLightHelper(dl, 3);
-    test.scene.add(dlHelper);
+    test.scene.add(dl, dlHelper);
 
-    // set up directional light settings
+    // set up directional light gui
     const dlSettings = {
       visible: true,
       color: dl.color.getHex(),
@@ -83,36 +83,45 @@ function App() {
     dlFolder.open();
 
     // set up spot light + helper
-    const sl = new THREE.SpotLight(0xffffff, 1, 8, Math.PI / 8, 0);
+    const sl = new THREE.SpotLight(0x00ff00, 1, 8, Math.PI / 8, 0);
     sl.position.set(0, 2, 2);
-    test.scene.add(sl);
     const slHelper = new THREE.SpotLightHelper(sl);
-    test.scene.add(slHelper);
+    test.scene.add(sl, slHelper);
 
+    // set up spot light gui
     const slSettings = {
       visible: true,
-      color: sl.color.getHex(),
     };
     const slFolder = gui.addFolder('spot light');
     slFolder.add(slSettings, 'visible').onChange((value) => {
       sl.visible = value;
       slHelper.visible = value;
     });
-    slFolder.add(sl, 'intensity', 0, 4);
-    slFolder.add(sl, 'angle', Math.PI / 16, Math.PI / 2);
+    slFolder.add(sl, 'intensity', 0, 4, 0.5);
+    slFolder.add(sl, 'angle', Math.PI / 16, Math.PI / 2, Math.PI / 16);
     slFolder.add(sl, 'castShadow');
-    slFolder
-      .addColor(slSettings, 'color')
-      .onChange((value) => sl.color.set(value));
     slFolder.open();
 
-    // spotLight.castShadow = true;
-    // spotLight.position.set(0, 1, 0);
-    // test.scene.add(spotLight);
-    // test.scene.add(spotLightHelper);
-    // const spot = gui.addFolder('spotlight');
-    // spot.add(spotLight, 'intensity', 0, 1);
-    // spot.open();
+    const pl = new THREE.PointLight(0xffffff, 2, 8, 2);
+    pl.position.set(2, 2, 2);
+    const plHelper = new THREE.PointLightHelper(pl, 1);
+    test.scene.add(pl, plHelper);
+
+    // set up point light gui
+    const plSettings = {
+      visible: true,
+      color: sl.color.getHex(),
+    };
+    const plFolder = gui.addFolder('point light');
+    plFolder.add(plSettings, 'visible').onChange((value) => {
+      pl.visible = value;
+      plHelper.visible = value;
+    });
+    plFolder.add(pl.position, 'x', -2, 2, 0.5);
+    plFolder.add(pl.position, 'y', -2, 2, 0.5);
+    plFolder.add(pl.position, 'z', -2, 2, 0.5);
+    plFolder.add(pl, 'castShadow');
+    plFolder.open();
   }, []);
 
   return (
--- 03-lighting/src/lib/SceneInit.js
+++ 03-lighting/src/lib/SceneInit.js
@@ -34,6 +34,7 @@ export default class SceneInit {
       1000
     );
     this.camera.position.z = 16;
+    this.camera.position.y = -1;
 
     // NOTE: Specify a canvas which is already created in the HTML.
     const canvas = document.getElementById(this.canvasId);
