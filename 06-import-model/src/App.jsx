93a4208aba53711f933316e4991dce3a7b4db59a
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-04-08 15:47:56 -0500

    Comment out code.

--- 06-import-model/src/App.jsx
+++ 06-import-model/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import SceneInit from './lib/SceneInit';
@@ -13,10 +14,34 @@ function App() {
     test.initialize();
     test.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    test.scene.add(boxMesh);
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    const glftLoader = new GLTFLoader();
+    let loadedModel;
+    glftLoader.load('./assets/shiba/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      console.log(loadedModel);
+      // Transform object
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });
+
+    // const animate = () => {
+    //   if (loadedModel) {
+    //     loadedModel.scene.scale.set(10, 10, 10);
+    //     loadedModel.scene.rotation.x += 0.01;
+    //     loadedModel.scene.rotation.y += 0.01;
+    //     loadedModel.scene.rotation.z += 0.01;
+    //   }
+    //   requestAnimationFrame(animate);
+    // };
+    // animate();
   }, []);
 
   return (
--- 06-import-model/src/lib/SceneInit.js
+++ 06-import-model/src/lib/SceneInit.js
@@ -52,12 +52,12 @@ export default class SceneInit {
     document.body.appendChild(this.stats.dom);
 
     // ambient light which is for the whole scene
-    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
     this.ambientLight.castShadow = true;
     this.scene.add(this.ambientLight);
 
     // directional light - parallel sun rays
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 2);
     // this.directionalLight.castShadow = true;
     this.directionalLight.position.set(0, 32, 64);
     this.scene.add(this.directionalLight);
