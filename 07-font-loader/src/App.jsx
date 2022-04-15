8cbf07ceb51cfd7c76c25ca6ed0e9f25999d52c9
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-04-15 15:37:03 -0500

    Set up code for font loader.

--- 07-font-loader/src/App.jsx
+++ 07-font-loader/src/App.jsx
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 
 import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 
 import SceneInit from './lib/SceneInit';
 
@@ -13,11 +14,51 @@ function App() {
     test.initialize();
     test.animate();
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // part 0 - comment out template code
+    // const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
 
-    test.scene.add(boxMesh);
+    // part 1 - typeface.json font loader
+    const fontLoader = new FontLoader();
+    fontLoader.load(
+      'node_modules/three/examples/fonts/droid/droid_serif_regular.typeface.json',
+      (droidFont) => {
+        const textGeometry = new TextGeometry('hello world', {
+          height: 2,
+          size: 10,
+          font: droidFont,
+        });
+        const textMaterial = new THREE.MeshNormalMaterial();
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.x = -36;
+        textMesh.position.y = 5;
+        test.scene.add(textMesh);
+      }
+    );
+
+    // part 2 - true type font loader
+    const ttfLoader = new TTFLoader();
+    ttfLoader.load(
+      // 'node_modules/three/examples/fonts/ttf/kenpixel.ttf',
+      'node_modules/three/examples/fonts/ttf/kenpixel.ttf',
+      (json) => {
+        // First parse the font.
+        const kenpixelFont = fontLoader.parse(json);
+        // Use parsed font as normal.
+        const textGeometry = new TextGeometry('hello world', {
+          height: 2,
+          size: 10,
+          font: kenpixelFont,
+        });
+        const textMaterial = new THREE.MeshNormalMaterial();
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.x = -50;
+        textMesh.position.y = -15;
+        test.scene.add(textMesh);
+      }
+    );
   }, []);
 
   return (
