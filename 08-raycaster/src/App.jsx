0d1b9a796974ae5e0b8a8acf2bd5c2ebc9c06432
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-04-29 14:59:16 -0500

    Add raycaster demo.

---
+++ 08-raycaster/src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect } from 'react';
+
+import * as THREE from 'three';
+
+import SceneInit from './lib/SceneInit';
+
+function App() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    // inital scene
+    // const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    const addNewBoxMesh = (x, y, z) => {
+      const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+      const boxMaterial = new THREE.MeshPhongMaterial({
+        color: 0xfafafa,
+      });
+      const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+      boxMesh.position.set(x, y, z);
+      test.scene.add(boxMesh);
+    };
+
+    // top rows
+    addNewBoxMesh(0, 2, 0);
+    addNewBoxMesh(2, 2, 0);
+    addNewBoxMesh(-2, 2, 0);
+    addNewBoxMesh(0, 2, -2);
+    addNewBoxMesh(2, 2, -2);
+    addNewBoxMesh(-2, 2, -2);
+    addNewBoxMesh(0, 2, 2);
+    addNewBoxMesh(2, 2, 2);
+    addNewBoxMesh(-2, 2, 2);
+
+    // middle rows
+    addNewBoxMesh(0, 0, 0);
+    addNewBoxMesh(2, 0, 0);
+    addNewBoxMesh(-2, 0, 0);
+    addNewBoxMesh(0, 0, -2);
+    addNewBoxMesh(2, 0, -2);
+    addNewBoxMesh(-2, 0, -2);
+    addNewBoxMesh(0, 0, 2);
+    addNewBoxMesh(2, 0, 2);
+    addNewBoxMesh(-2, 0, 2);
+
+    // bottom rows
+    addNewBoxMesh(0, -2, 0);
+    addNewBoxMesh(2, -2, 0);
+    addNewBoxMesh(-2, -2, 0);
+    addNewBoxMesh(0, -2, -2);
+    addNewBoxMesh(2, -2, -2);
+    addNewBoxMesh(-2, -2, -2);
+    addNewBoxMesh(0, -2, 2);
+    addNewBoxMesh(2, -2, 2);
+    addNewBoxMesh(-2, -2, 2);
+
+    const pointer = new THREE.Vector2();
+    const raycaster = new THREE.Raycaster();
+
+    const onMouseMove = (event) => {
+      // calculate pointer position in normalized device coordinates
+      // (-1 to +1) for both components
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      raycaster.setFromCamera(pointer, test.camera);
+      const intersects = raycaster.intersectObjects(test.scene.children);
+
+      for (let i = 0; i < intersects.length; i++) {
+        console.log(intersects);
+      }
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
+
+    // change color of objects intersecting  the raycaster
+    // for (let i = 0; i < intersects.length; i++) {
+    //   intersects[i].object.material.color.set(0xff0000);
+    // }
+
+    // change color of the closest object intersecting the raycaster
+    // if (intersects.length > 0) {
+    //   intersects[0].object.material.color.set(0xff0000);
+    // }
+  }, []);
+
+  return (
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
+}
+
+export default App;
