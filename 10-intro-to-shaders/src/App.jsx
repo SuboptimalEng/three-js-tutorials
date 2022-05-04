d9c07821cf28082284f077a1b1323932559e20c2
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-05-04 17:23:48 -0500

    Comment out code for the demo.

--- 10-intro-to-shaders/src/App.jsx
+++ 10-intro-to-shaders/src/App.jsx
@@ -13,7 +13,12 @@ function App() {
     const axesHelper = new THREE.AxesHelper(16);
     test.scene.add(axesHelper);
 
-    // part 1 - boilerplate code
+    // const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    // // part 1 - boilerplate code
     // const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16);
     // const boxMaterial = new THREE.MeshStandardMaterial({
     //   color: 0xff0000,
@@ -44,36 +49,37 @@ function App() {
     // test.scene.add(boxMesh);
 
     // part 3 - basics of glsl shaders
-    // const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16);
-    // const boxMaterial = new THREE.ShaderMaterial({
-    //   wireframe: true,
-    //   vertexShader: `
-    //   void main()	{
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, position.y, position.z, 1.0);
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z), position.z, 1.0);
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
-    //     gl_Position = projectionMatrix
-    //       * modelViewMatrix
-    //       * vec4(position.x, 4.0*sin(position.z/2.0) + position.y, position.z, 1.0);
-    //   }
-    //   `,
-    //   fragmentShader: `
-    //   void main() {
-    //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    //   }
-    //   `,
-    // });
-    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    // test.scene.add(boxMesh);
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16);
+    const boxMaterial = new THREE.ShaderMaterial({
+      wireframe: true,
+      vertexShader: `
+      void main()	{
+        // gl_Position = projectionMatrix
+        //   * modelViewMatrix
+        //   * vec4(position.x, position.y, position.z, 1.0);
+        // gl_Position = projectionMatrix
+        //   * modelViewMatrix
+        //   * vec4(position.x, sin(position.z), position.z, 1.0);
+        // gl_Position = projectionMatrix
+        //   * modelViewMatrix
+        //   * vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
+        // gl_Position = projectionMatrix
+        //   * modelViewMatrix
+        //   * vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
+        gl_Position = projectionMatrix
+          * modelViewMatrix
+          * vec4(position.x, 4.0*sin(position.z/4.0) + position.y, position.z, 1.0);
+      }
+      `,
+      fragmentShader: `
+      void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+      }
+      `,
+    });
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    test.scene.add(boxMesh);
   }, []);
 
   return (
