94d746f931c2b7922ba3c6694bc369cf158d0569
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-05-06 13:19:11 -0500

    Clean up code.

--- 11-shaders-part-02/src/App.jsx
+++ 11-shaders-part-02/src/App.jsx
@@ -14,56 +14,159 @@ function App() {
     test.scene.add(axesHelper);
 
     // part 1 - boilerplate code
-    const boxGeometry = new THREE.BoxGeometry(16, 4, 16, 16, 4, 16);
-    const boxMaterial = new THREE.MeshStandardMaterial({
-      color: 0xff0000,
-      wireframe: true,
-    });
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    test.scene.add(boxMesh);
+    // const boxGeometry = new THREE.BoxGeometry(24, 4, 24, 24, 4, 24);
+    // const boxMaterial = new THREE.MeshStandardMaterial({
+    //   color: 0xff0000,
+    //   wireframe: true,
+    // });
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
 
     // basic glsl shaders
-    // const boxGeometry = new THREE.BoxGeometry(16, 8, 16, 16, 8, 16);
+    // const boxGeometry = new THREE.BoxGeometry(24, 4, 24, 24, 4, 24);
     // const boxMaterial = new THREE.ShaderMaterial({
     //   wireframe: true,
     //   vertexShader: `
     //   void main()	{
+    //     vec4 result;
     //     // // re-write boiler plate code with shader
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, position.y, position.z, 1.0);
-    //     // // convert box into sine wave
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z), position.z, 1.0);
-    //     // // make the box wavy
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
-    //     // // change how wavy the box by updating frequency
-    //     // gl_Position = projectionMatrix
-    //     //   * modelViewMatrix
-    //     //   * vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
+    //     result = vec4(position.x, position.y, position.z, 1.0);
+    //     // // convert box into a 2D sine wave plane
+    //     // result = vec4(position.x, sin(position.z), position.z, 1.0);
+    //     // // change the 2D sine wave plane into a wavy box
+    //     // result = vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
+    //     // // change how wavy the box is by updating frequency
+    //     // result = vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
     //     // // change the amplitude of the box's waves
+    //     // result = vec4(position.x, 4.0*sin(position.z/4.0) + position.y, position.z, 1.0);
+    //     // re-write boiler plate code with shader
     //     gl_Position = projectionMatrix
     //       * modelViewMatrix
-    //       * vec4(position.x, 4.0*sin(position.z/4.0) + position.y, position.z, 1.0);
+    //       * result;
     //   }
     //   `,
     //   fragmentShader: `
-    //   varying vec3 pos;
+    //   // varying vec3 pos;
     //   void main() {
     //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    //     // // change the color of the box to green
-    //     // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
     //   }
     //   `,
     // });
     // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     // test.scene.add(boxMesh);
 
-    // varying variable
-    // const boxGeometry = new THREE.BoxGeometry(16, 8, 16, 16, 8, 16);
+    const uniformData = {
+      u_time: {
+        type: 'f',
+        value: test.clock.getElapsedTime(),
+      },
+    };
+    const render = () => {
+      uniformData.u_time.value = test.clock.getElapsedTime();
+      window.requestAnimationFrame(render);
+    };
+    render();
+
+    // uniform variables
+    // const boxGeometry = new THREE.BoxGeometry(24, 4, 24, 24, 4, 24);
+    // const boxMaterial = new THREE.ShaderMaterial({
+    //   wireframe: true,
+    //   // uniforms: uniformData,
+    //   vertexShader: `
+    //   // uniform float u_time;
+
+    //   void main()	{
+    //     vec4 result;
+
+    //     // re-write boiler plate code with shader
+    //     result = vec4(position.x, position.y, position.z, 1.0);
+    //     // result = vec4(position.x, position.y + sin(u_time), position.z, 1.0);
+
+    //     // convert box into a 2D sine wave plane
+    //     // result = vec4(position.x, sin(position.z), position.z, 1.0);
+    //     // result = vec4(position.x, sin(position.z + u_time), position.z, 1.0);
+
+    //     // change the 2D sine wave plane into a wavy box
+    //     // result = vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
+    //     // result = vec4(position.x, sin(position.z + u_time) + position.y, position.z, 1.0);
+
+    //     // change how wavy the box is by updating frequency
+    //     // result = vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
+    //     // result = vec4(position.x, sin((position.z)/4.0 + u_time) + position.y, position.z, 1.0);
+
+    //     // change the amplitude of the box's waves
+    //     // result = vec4(position.x, 4.0*sin(position.z/4.0) + position.y, position.z, 1.0);
+    //     // result = vec4(position.x, 4.0*sin(position.z/4.0 + u_time) + position.y, position.z, 1.0);
+
+    //     gl_Position = projectionMatrix
+    //       * modelViewMatrix
+    //       * result;
+    //   }
+    //   `,
+    //   fragmentShader: `
+    //   // uniform float u_time;
+
+    //   void main() {
+    //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    //     // gl_FragColor = vec4(sin(u_time), 0.0, 0.0, 1.0);
+    //     // gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
+    //   }
+    //   `,
+    // });
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    // varying variables
+    const boxGeometry = new THREE.BoxGeometry(24, 4, 24, 24, 4, 24);
+    const boxMaterial = new THREE.ShaderMaterial({
+      wireframe: true,
+      uniforms: uniformData,
+      vertexShader: `
+      uniform float u_time;
+
+      varying vec3 pos;
+
+      void main()	{
+        vec4 result;
+
+        pos = position;
+
+        // result = vec4(
+        //   position.x,
+        //   4.0*sin(position.z/4.0) + position.y,
+        //   position.z,
+        //   1.0
+        // );
+        result = vec4(
+          position.x,
+          4.0*sin(position.z/4.0 + u_time) + position.y,
+          position.z,
+          1.0
+        );
+
+        gl_Position = projectionMatrix
+          * modelViewMatrix
+          * result;
+      }
+      `,
+      fragmentShader: `
+      varying vec3 pos;
+      uniform float u_time;
+      void main() {
+        if (pos.x >= 0.0) {
+          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+          // gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
+        } else {
+          gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+          // gl_FragColor = vec4(0.0, abs(cos(u_time)), 0.0, 1.0);
+        }
+      }
+      `,
+    });
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    test.scene.add(boxMesh);
+    // varying variables
+    // const boxGeometry = new THREE.BoxGeometry(24, 8, 24, 24, 8, 24);
     // const boxMaterial = new THREE.ShaderMaterial({
     //   wireframe: true,
     //   vertexShader: `
