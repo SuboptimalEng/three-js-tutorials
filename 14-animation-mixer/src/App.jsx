b6fe08aad48617cbfc5fcce73c753234f3d0d325
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-05-12 16:09:50 -0500

    Rename 14th tutorial folder.

--- 14-animation-mixer/src/App.jsx
+++ 14-animation-mixer/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 import SceneInit from './lib/SceneInit';
 
@@ -20,9 +21,12 @@ function App() {
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     test.scene.add(boxMesh);
 
-    // part 2 - group example
+    const fbxLoader = new FBXLoader();
 
-    // part 3 - solar system example
+    fbxLoader.load('./assets/dancing.fbx', (model) => {
+      model.scale.set(0.1, 0.1, 0.1);
+      test.scene.add(model);
+    });
   }, []);
 
   return (
