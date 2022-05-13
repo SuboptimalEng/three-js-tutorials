baad969c3b390c2ead0358f6617756fb5a335c24
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-05-13 10:40:03 -0500

    Remove 14th tutorial folder.

--- 14-animation-mixer/vite.config.js
+++
@@ -1,8 +0,0 @@
-import glsl from 'vite-plugin-glsl';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), glsl()],
-});
