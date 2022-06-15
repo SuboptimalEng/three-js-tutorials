ddcc19adc70167b35ff9ce09d1cc1256a3a960f9
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-06-15 12:37:08 -0500

    Set up rigid vehicle demo.

--- 16-physics-part-02/src/App.jsx
+++ 16-physics-part-02/src/App.jsx
@@ -17,7 +17,8 @@ function App() {
     test.scene.add(axesHelper);
 
     // note - code is from the cannon-es documentation
-    // set up world physics with a few objects
+    // code - https://github.com/pmndrs/cannon-es/blob/master/examples/rigid_vehicle.html
+    // set up world physics with gravity
     const physicsWorld = new CANNON.World({
       gravity: new CANNON.Vec3(0, -9.82, 0),
     });
@@ -29,48 +30,154 @@ function App() {
       shape: new CANNON.Plane(),
     });
     // rotate ground body by 90 degrees
-    groundBody.quaternion.setFromEuler(-Math.PI / 2, Math.PI / 16, 0);
+    groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    // groundBody.quaternion.setFromEuler(-Math.PI / 2, Math.PI / 24, 0);
     physicsWorld.addBody(groundBody);
 
-    // create a sphere and set it at y=10
-    const radius = 1;
-    const sphereBody = new CANNON.Body({
+    // add a green wireframe to each object and visualize the physics world
+    const cannonDebugger = new CannonDebugger(test.scene, physicsWorld);
+
+    // part 1 - add base vehicle
+    // add car body
+    const carBody = new CANNON.Body({
       mass: 5,
-      shape: new CANNON.Sphere(radius),
+      position: new CANNON.Vec3(0, 6, 0),
+      shape: new CANNON.Box(new CANNON.Vec3(4, 0.5, 2)),
     });
-    sphereBody.position.set(0, 7, 0);
-    physicsWorld.addBody(sphereBody);
+    // physicsWorld.addBody(carBody);
 
-    // add a box object
-    const boxBody = new CANNON.Body({
-      mass: 5,
-      shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    const vehicle = new CANNON.RigidVehicle({
+      chassisBody: carBody,
     });
-    boxBody.position.set(0.2, 10, 0);
-    physicsWorld.addBody(boxBody);
 
-    // bind cannon debugger to the three.js scene + physics world
-    const cannonDebugger = new CannonDebugger(test.scene, physicsWorld, {});
+    // part 2 - add wheels
+    const mass = 1;
+    const axisWidth = 5;
+    const wheelShape = new CANNON.Sphere(1);
+    const wheelMaterial = new CANNON.Material('wheel');
+    const down = new CANNON.Vec3(0, -1, 0);
 
-    // combine the three.js game world with the physics world
-    const geometry = new THREE.SphereGeometry(radius);
-    const material = new THREE.MeshNormalMaterial();
-    const sphereMesh = new THREE.Mesh(geometry, material);
-    test.scene.add(sphereMesh);
+    const wheelBody1 = new CANNON.Body({ mass, material: wheelMaterial });
+    wheelBody1.addShape(wheelShape);
+    wheelBody1.angularDamping = 0.4;
+    vehicle.addWheel({
+      body: wheelBody1,
+      position: new CANNON.Vec3(-2, 0, axisWidth / 2),
+      axis: new CANNON.Vec3(0, 0, 1),
+      direction: down,
+    });
 
-    // combine the three.js game world with the physics world
-    const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const wheelBody2 = new CANNON.Body({ mass, material: wheelMaterial });
+    wheelBody2.addShape(wheelShape);
+    wheelBody2.angularDamping = 0.4;
+    vehicle.addWheel({
+      body: wheelBody2,
+      position: new CANNON.Vec3(-2, 0, -axisWidth / 2),
+      axis: new CANNON.Vec3(0, 0, 1),
+      direction: down,
+    });
+
+    const wheelBody3 = new CANNON.Body({ mass, material: wheelMaterial });
+    wheelBody3.addShape(wheelShape);
+    wheelBody3.angularDamping = 0.4;
+    vehicle.addWheel({
+      body: wheelBody3,
+      position: new CANNON.Vec3(2, 0, axisWidth / 2),
+      axis: new CANNON.Vec3(0, 0, 1),
+      direction: down,
+    });
+
+    const wheelBody4 = new CANNON.Body({ mass, material: wheelMaterial });
+    wheelBody4.addShape(wheelShape);
+    wheelBody4.angularDamping = 0.4;
+    vehicle.addWheel({
+      body: wheelBody4,
+      position: new CANNON.Vec3(2, 0, -axisWidth / 2),
+      axis: new CANNON.Vec3(0, 0, 1),
+      direction: down,
+    });
+
+    vehicle.addToWorld(physicsWorld);
+
+    // part 3 - handle user input
+    document.addEventListener('keydown', (event) => {
+      const maxSteerVal = Math.PI / 8;
+      const maxSpeed = 10;
+      const maxForce = 10;
+
+      switch (event.key) {
+        case 'w':
+        case 'ArrowUp':
+          vehicle.setWheelForce(maxForce, 0);
+          vehicle.setWheelForce(maxForce, 1);
+          break;
+
+        case 's':
+        case 'ArrowDown':
+          vehicle.setWheelForce(-maxForce / 2, 0);
+          vehicle.setWheelForce(-maxForce / 2, 1);
+          break;
+
+        case 'a':
+        case 'ArrowLeft':
+          vehicle.setSteeringValue(maxSteerVal, 0);
+          vehicle.setSteeringValue(maxSteerVal, 1);
+          break;
+
+        case 'd':
+        case 'ArrowRight':
+          vehicle.setSteeringValue(-maxSteerVal, 0);
+          vehicle.setSteeringValue(-maxSteerVal, 1);
+          break;
+      }
+    });
+
+    document.addEventListener('keyup', (event) => {
+      switch (event.key) {
+        case 'w':
+        case 'ArrowUp':
+          vehicle.setWheelForce(0, 0);
+          vehicle.setWheelForce(0, 1);
+          break;
+
+        case 's':
+        case 'ArrowDown':
+          vehicle.setWheelForce(0, 0);
+          vehicle.setWheelForce(0, 1);
+          break;
+
+        case 'a':
+        case 'ArrowLeft':
+          vehicle.setSteeringValue(0, 0);
+          vehicle.setSteeringValue(0, 1);
+          break;
+
+        case 'd':
+        case 'ArrowRight':
+          vehicle.setSteeringValue(0, 0);
+          vehicle.setSteeringValue(0, 1);
+          break;
+      }
+    });
+
+    // sync game world with physics world
+    const boxGeometry = new THREE.BoxGeometry(8, 1, 4);
     const boxMaterial = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     test.scene.add(boxMesh);
 
+    const sphereGeometry1 = new THREE.SphereGeometry(1);
+    const sphereMaterial1 = new THREE.MeshNormalMaterial();
+    const sphereMesh1 = new THREE.Mesh(sphereGeometry1, sphereMaterial1);
+    test.scene.add(sphereMesh1);
+
     const animate = () => {
       physicsWorld.fixedStep();
       cannonDebugger.update();
-      boxMesh.position.copy(boxBody.position);
-      boxMesh.quaternion.copy(boxBody.quaternion);
-      sphereMesh.position.copy(sphereBody.position);
-      sphereMesh.quaternion.copy(sphereBody.quaternion);
+      boxMesh.position.copy(carBody.position);
+      boxMesh.quaternion.copy(carBody.quaternion);
+      sphereMesh1.position.copy(wheelBody1.position);
+      sphereMesh1.quaternion.copy(wheelBody1.quaternion);
       window.requestAnimationFrame(animate);
     };
     animate();
